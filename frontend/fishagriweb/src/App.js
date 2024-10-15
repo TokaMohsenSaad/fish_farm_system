15843d929a73ff4e7c2757cc1cc88fd5f1084cd4
Toka Mohsen <tokasaad2012@gmail.com>
2024-10-15 23:54:45 +0300

    control almost done

--- frontend/fishagriweb/src/App.js
+++ frontend/fishagriweb/src/App.js
@@ -1,26 +1,31 @@
-import './App.css';
-import { React ,useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Navbar from './components/navbar';
-import Discover from './discover';
-import Home from './home';
-import Control from './control';
-import History from './history';
-import Signup from './components/signup';
-import Login from './components/login';
-import Admin from './Admin';
+import "./App.css";
+import { React, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Navbar from "./components/navbar";
+import Discover from "./discover";
+import Home from "./home";
+import Control from "./control";
+import History from "./history";
+import Signup from "./components/signup";
+import Login from "./components/login";
+import Admin from "./Admin";
 
 function App() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
   return (
     <Router>
       <Navbar firstName={firstName} lastName={lastName} email={email} />
       <div>
         <Routes>
-          <Route path="/" element={<Login />} /> 
-          <Route path="/home" element={<Home/>} />
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
           {/* based on database after login navigate to home */}
           <Route path="/discover" element={<Discover />} />
           <Route path="/control" element={<Control />} />
